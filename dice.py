1fba5c1c6a20ec539162de0c5b3ba1f85d0a7baf
joacosando <joaquinsandovalreyes04@gmail.com>
2025-08-26 16:38:46 -0400

    RED: test para las pintas de los dados - falla como esperado

--- dice.py
+++ dice.py
@@ -5,3 +5,5 @@ class Dice:
         pass
     def roll_dice(self):
         return random.randint(1,6)
+    def pinta(self, int):
+        pass
--- tests/test_dice.py
+++ tests/test_dice.py
@@ -1,3 +1,5 @@
+from unittest import mock
+import pytest
 from dice import Dice
 
 def test_roll_dice():
@@ -5,3 +7,8 @@ def test_roll_dice():
     roll_dice = dice.roll_dice()
     assert roll_dice in [1,2,3,4,5,6]
 
+@pytest.mark.parametrize("_input, expected", [(1, "As"), (2, "Tonto"), (3, "Tren"), (4, "Cuadra"), (5, "Quina"), (6, "Sexto")])
+@mock.patch("dice.Dice.roll_dice")
+def test_pinta(mock_roll_dice, _input, expected):
+    dice = Dice()
+    assert dice.pinta(_input) == expected
