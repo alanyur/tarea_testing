dff0398c459b8a0236d51daa3931c452141723f7
joacosando <joaquinsandovalreyes04@gmail.com>
2025-08-28 14:58:59 -0400

    GREEN: implementacion minima para crear dados en un cacho

--- cacho.py
+++ cacho.py
@@ -1,7 +1,10 @@
+from dice import Dice
+
 class Cacho:
     dados = [0]*5
     def __init__(self):
-        pass
+        for i in range(5):
+            self.dados[i] = Dice()
 
     def agitar(self):
         pass
