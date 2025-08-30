0274e266681b136f46b170935280ddb333697203
Alan <alanibagon@gmail.com>
2025-08-30 13:58:46 -0400

    GREEN: implementación mínima para administrar jugadores y sus dados

--- src/juego/gestor_partida.py
+++ src/juego/gestor_partida.py
@@ -2,5 +2,10 @@ import random
 from src.juego import cacho
 class gestor:
     def __init__(self):
-        pass
+        cantidad_jugadores=int(input("ingrese la cantidad de jugadores (2-5): "))
+        while cantidad_jugadores <2 or cantidad_jugadores>5:
+            cantidad_jugadores=int(input("ingrese la cantidad de jugadores (2-5): "))
+        self.jugadores = [cacho.Cacho() for i in range(cantidad_jugadores)]
+        
+
     
