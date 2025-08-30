3dc7c5aee2cdf2230fd5f3168ebe99ff048f1da6
Alan <alanibagon@gmail.com>
2025-08-30 14:09:10 -0400

    GREEN: implementación mínima para decidir quien inicia cada ronda

--- src/juego/gestor_partida.py
+++ src/juego/gestor_partida.py
@@ -6,7 +6,12 @@ class gestor:
         while cantidad_jugadores <2 or cantidad_jugadores>5:
             cantidad_jugadores=int(input("ingrese la cantidad de jugadores (2-5): "))
         self.jugadores = [cacho.Cacho() for i in range(cantidad_jugadores)]
-        
+        self.iniciar_juego()
 
     def iniciar_juego(self):
-        pass
+        # Elegir al azar quién comienza
+        self.turno_actual = random.randint(0, len(self.jugadores) - 1)
+        print(f"Inicia jugador {self.turno_actual + 1}")
+
+   
+  
